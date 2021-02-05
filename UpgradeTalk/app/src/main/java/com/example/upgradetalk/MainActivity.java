4e7a790ac0e5cc56a79998520407215eb50d210d
leehandsub <gustjq7027@naver.com>
2021-02-06 01:34:49 +0900

    create_fragment

--- UpgradeTalk/app/src/main/java/com/example/upgradetalk/MainActivity.java
+++ UpgradeTalk/app/src/main/java/com/example/upgradetalk/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.upgradetalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -19,37 +23,21 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 
+import me.relex.circleindicator.CircleIndicator3;
+
 public class MainActivity extends AppCompatActivity {
     private LineChart chart;
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 2;
+    private CircleIndicator3 mIndicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chart = findViewById(R.id.linechart);
 
-        ArrayList<Entry> values = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-
-            float val = (float) (Math.random() * 10);
-            values.add(new Entry(i, val));
-        }
-
-        LineDataSet set1;
-        set1 = new LineDataSet(values, "DataSet 1");
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1); // add the data sets
-
-        // create a data object with the data sets
-        LineData data = new LineData(dataSets);
-
-        // black lines and points
-        set1.setColor(Color.BLACK);
-        set1.setCircleColor(Color.BLACK);
-
-        // set data
-        chart.setData(data);
+        createChart();
+        createFragment();
 
     }
     public void save_dialog(View v) {
@@ -78,6 +66,86 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.dismiss();
             }
         });
+    }
+
+    public void createChart() {
+        chart = findViewById(R.id.linechart);
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            float val = (float) (Math.random() * 10);
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+        set1 = new LineDataSet(values, "DataSet 1");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the data sets
+
+        // create a data object with the data sets
+        LineData data = new LineData(dataSets);
+
+        // black lines and points
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+
+        // set data
+        chart.setData(data);
+    }
+
+    public void createFragment(){
+        mPager = findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(1000);
+        mPager.setOffscreenPageLimit(3);
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
+            }
+
+        });
+
+        final float pageMargin= getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
+
+//        mPager.setPageTransformer(new ViewPager2.PageTransformer() {
+//            @Override
+//            public void transformPage(@NonNull View page, float position) {
+//                float myOffset = position * -(2 * pageOffset + pageMargin);
+//                if (mPager.getOrientation() == ViewPager2.ORIENTATION_HORIZONTAL) {
+//                    if (ViewCompat.getLayoutDirection(mPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+//                        page.setTranslationX(-myOffset);
+//                    } else {
+//                        page.setTranslationX(myOffset);
+//                    }
+//                } else {
+//                    page.setTranslationY(myOffset);
+//                }
+//            }
+//        });
+
     }
 
 }
